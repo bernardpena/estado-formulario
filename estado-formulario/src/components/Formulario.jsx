572fe4f8e8b727a30b7799ca806b643dd2294739
Bernardo <bernardpena@gmail.com>
2024-04-26 00:04:34 -0400

    Diseño Formulario

--- estado-formulario/src/components/Formulario.jsx
+++ estado-formulario/src/components/Formulario.jsx
@@ -2,29 +2,37 @@ import React from 'react'
 
 export default function Formulario() {
     return (
-        <div>
-            <form>
-                <div className="mb-3 formIngreso">
-                    <h1>Crea una Cuenta</h1>
+            <form className='formIngreso'>
+                <div className="container">
+                <div className="mb-3 formIngresoDiv">
+                    <h1 className='titulo' >Crea una Cuenta</h1>
                     <div>
-                        <h3>Iconos</h3>
+                        <h3 className='iconos'>Iconos</h3>
                     </div>
                     <div>
-                        <h4>o usa tu email para registrarte</h4>
+                        <h4 className='mensaje'>o usa tu email para registrarte</h4>
                     </div>
-                    <div>
-                        <input type="text" className='form-control nombre' />
+                    <div className="mb-3 infoPersona">
+                        <div className="mb-3">
+                            <input type="text" className='form-control nombre' />
+                        </div>
+                        <div className="mb-3">
+                            <input type="email" className="form-control email" aria-describedby="emailHelp" />
+                        </div>
                     </div>
-                    <input type="email" className="form-control email" aria-describedby="emailHelp" />
 
                 </div>
                 <div className="mb-3">
-
                     <input type="password" className="form-control password" />
+                 </div>
+                 <div className="mb-3">
                     <input type="password" className="form-control confirmPassword" />
-                </div>
+                 </div>
+                <div className="mb-3">
                 <button type="submit" className="btn btn-primary">Enviar</button>
+                </div>
+                </div>
             </form>
-        </div>
+            
     )
 }
