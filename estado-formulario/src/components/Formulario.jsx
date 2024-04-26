990ae01b429475b92c6941ee4761979832dc2ae1
Bernardo <bernardpena@gmail.com>
2024-04-26 01:19:48 -0400

    Diseño Form Listo

--- estado-formulario/src/components/Formulario.jsx
+++ estado-formulario/src/components/Formulario.jsx
@@ -2,37 +2,42 @@ import React from 'react'
 
 export default function Formulario() {
     return (
-            <form className='formIngreso'>
-                <div className="container">
+        <form className='formIngreso'>
+            <div className="container">
                 <div className="mb-3 formIngresoDiv">
                     <h1 className='titulo' >Crea una Cuenta</h1>
                     <div>
-                        <h3 className='iconos'>Iconos</h3>
+                        <div className="footer-icon">
+                            <a href="#"><i className="fa-brands fa-facebook"></i></a>
+                            <a href="#"><i className="fa-brands fa-instagram"></i></a>
+                            <a href="#"><i className="fa-brands fa-linkedin"></i></a>
+                        </div>
                     </div>
                     <div>
                         <h4 className='mensaje'>o usa tu email para registrarte</h4>
                     </div>
                     <div className="mb-3 infoPersona">
                         <div className="mb-3">
-                            <input type="text" className='form-control nombre' />
+                            <input type="text" placeholder="Nombre" className='form-control nombre' />
                         </div>
                         <div className="mb-3">
-                            <input type="email" className="form-control email" aria-describedby="emailHelp" />
+                            <input type="email" placeholder="tuemail@ejemplo.com" className="form-control email" aria-describedby="emailHelp" />
                         </div>
                     </div>
 
+
+                    <div className="mb-3 infoPass">
+                        <input type="password" placeholder="contraseña" className='form-control password' />
+                    </div>
+                    <div className="mb-3">
+                        <input type="password" placeholder="confirme contraseña" className="form-control confirmPassword" />
+                    </div>
+                    <div className="mb-3 d-grid gap-2">
+                        <button type="submit" className="btn btn-success">Registrarse</button>
+                    </div>
                 </div>
-                <div className="mb-3">
-                    <input type="password" className="form-control password" />
-                 </div>
-                 <div className="mb-3">
-                    <input type="password" className="form-control confirmPassword" />
-                 </div>
-                <div className="mb-3">
-                <button type="submit" className="btn btn-primary">Enviar</button>
-                </div>
-                </div>
-            </form>
-            
+            </div>
+        </form>
+
     )
 }
