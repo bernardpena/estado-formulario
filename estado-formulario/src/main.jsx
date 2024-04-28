dfe99928098ee6781017a996e5f500e559fbef96
Bernardo <bernardpena@gmail.com>
2024-04-27 22:39:38 -0400

    modificaciones Alert

--- estado-formulario/src/main.jsx
+++ estado-formulario/src/main.jsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import "bootstrap/dist/css/bootstrap.min.css";
-import Swal from 'sweetalert2'
-import withReactContent from 'sweetalert2-react-content'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
