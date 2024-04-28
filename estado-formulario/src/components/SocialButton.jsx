a2abd68b28f9b80001c6313b54d3c91da436bfea
Bernardo <bernardpena@gmail.com>
2024-04-27 22:57:42 -0400

    afinando detalles para subir

--- estado-formulario/src/components/SocialButton.jsx
+++ estado-formulario/src/components/SocialButton.jsx
@@ -14,17 +14,3 @@ const SocialButtons = (props) => {
 }
 
 export default SocialButtons;
-
-
-
-// export default function SocialButton() {
-//     return (
-//         <div>
-//             <div className="footer-icon">
-//                 <a href="#"><i className="fa-brands fa-facebook"></i></a>
-//                 <a href="#"><i className="fa-brands fa-instagram"></i></a>
-//                 <a href="#"><i className="fa-brands fa-linkedin"></i></a>
-//             </div>
-//         </div>
-//     )
-// }
